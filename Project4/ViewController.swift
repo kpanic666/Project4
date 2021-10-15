aea42b5317ecd089f406b3039f8e82409bd1d963
Andrei Korikov <kpanic666@gmail.com>
2021-10-15 22:30:44 +0500

    Initial Commit

---
+++ Project4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project4
+//
+//  Created by Andrei Korikov on 15.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
